99bc2c9c34348dd79fed03d1116e6a0db1359cc7
Mark Schall <mark@detroitlabs.com>
2015-07-29 22:22:55 -0400

    get rid of subcommands for now

--- lib/<%= project_name %>/cli.rb
+++ lib/<%= project_name %>/cli.rb
@@ -8,18 +8,9 @@ module <%= project_name.capitalize %>
     def cli()
       if options[:version]
         puts "Version: #{VERSION}"
-      elsif !options[:help]
-        # YOUR DEFAULT CODE GOES HERE
       else
         CLI.help
       end
-    end
-  
-    Dir[File.expand_path('../subcommands/*.rb', __FILE__)].each do |file|
-      filename = File.basename(file, ".rb")
-      require_relative "subcommands/#{filename}"
-      desc "#{filename} [COMMANDS]", ""
-      subcommand filename, <%= project_name.capitalize %>.const_get(filename.capitalize)
     end
     
     default_task :cli
